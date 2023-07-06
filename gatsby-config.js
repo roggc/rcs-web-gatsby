8b1914bfe6f748a2c55aeb5b55c45a3678786088
roggc <roggc9@gmail.com>
2023-07-06 10:25:47 +0200

    solve rehydration error and use gatsby sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,62 @@
+const siteUrl = process.env.URL || "https://reactcontextslices.gatsbyjs.io";
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
   siteMetadata: {
     title: `react-context-slices`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        query: `
+      {
+        allSitePage {
+          nodes {
+            path
+          }
+        }
+        allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
+          nodes {
+            ... on WpPost {
+              uri
+              modifiedGmt
+            }
+            ... on WpPage {
+              uri
+              modifiedGmt
+            }
+          }
+        }
+      }
+    `,
+        resolveSiteUrl: () => siteUrl,
+        resolvePages: ({
+          allSitePage: { nodes: allPages },
+          allWpContentNode: { nodes: allWpNodes },
+        }) => {
+          const wpNodeMap = allWpNodes.reduce((acc, node) => {
+            const { uri } = node;
+            acc[uri] = node;
+
+            return acc;
+          }, {});
+
+          return allPages.map((page) => {
+            return { ...page, ...wpNodeMap[page.path] };
+          });
+        },
+        serialize: ({ path, modifiedGmt }) => {
+          return {
+            url: path,
+            lastmod: modifiedGmt,
+          };
+        },
+      },
+    },
+  ],
 };
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -8,14 +8,24 @@ import MobileLateralMenu from "../mobile-lateral-menu/mobile-lateral-menu";
 
 const Layout = ({ children, location }) => {
   const isDesktopOrTablet = useMediaQuery({ minWidth: 850 });
-  return isDesktopOrTablet ? (
-    <div className={styles.appContainer}>
+  const [isDomLoaded, setIsDomLoaded] = React.useState(false);
+  React.useEffect(() => {
+    setIsDomLoaded(true);
+  }, []);
+  return isDesktopOrTablet || !isDomLoaded ? (
+    <div
+      className={styles.appContainer}
+      style={isDomLoaded ? undefined : { visibility: "hidden" }}
+    >
       <LateralMenu location={location} />
       <div className={styles.pageContent}>{children}</div>
     </div>
   ) : (
     <>
-      <div className={`${styles.appContainer} ${styles.mobileAppContainer}`}>
+      <div
+        className={`${styles.appContainer} ${styles.mobileAppContainer}`}
+        style={isDomLoaded ? undefined : { visibility: "hidden" }}
+      >
         <Header />
         <div className={styles.mobilePageContent}>
           <MobileLateralMenu location={location} />
